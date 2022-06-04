ae2872b9376699b88f117c50d641d4cb9f8da9e2
codewithdhruv22 <codewithdhruv22@gmail.com>
2022-06-04 14:33:34 +0530

    Video No 4

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask , render_template
+import urllib
+import json
 app = Flask(__name__)
 
 
@@ -7,8 +9,15 @@ def home():
     print("HELLO I AM A FLASK APP")
     return render_template("index.html" , page_name = "WHAT2WATCH")
 
-@app.route("/search")
-def search_results():
-    return render_template("index.html" , page_name = "SEARCH RESULTS")
+
+@app.route("/search/<string:movieName>")
+def search_results(movieName):
+    url =  f"http://www.omdbapi.com/?s={movieName}&apikey=c4b3701e"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    jsonData = json.loads(data)["Search"]
+    return render_template("index.html" , page_name = "SEARCH RESULTS" , movieList = jsonData)
+
+
 
 app.run(debug =  True)
