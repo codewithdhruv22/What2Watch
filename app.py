2a702468499ba1ea12f969d4d4b2997d32972d28
codewithdhruv22 <codewithdhruv22@gmail.com>
2022-06-13 22:16:23 +0530

    Video No 5

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 import urllib
+import random
 import json
 app = Flask(__name__)
 
@@ -7,16 +8,37 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("HELLO I AM A FLASK APP")
-    return render_template("index.html" , page_name = "WHAT2WATCH")
+    movieName = ["Avengers" , "Don" , "India" , "Space"]
+    myMovieName = random.choice(movieName)
+    query = random.choice(["Avengers" , "Don" , "India" , "Space" , "Cricket"  , "Mera Badla" , "DJ"])
+    try:
+        url =  f"http://www.omdbapi.com/?s={myMovieName}&apikey=c4b3701e"
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        jsonData = json.loads(data)["Search"]
+        return render_template("index.html" , page_name = "WHAT2WATCH" , movieList  = jsonData , query =query)
+    except Exception as e:
+
+        return render_template("index.html" , page_name = "WHAT2WATCH")
 
 
-@app.route("/search/<string:movieName>")
-def search_results(movieName):
-    url =  f"http://www.omdbapi.com/?s={movieName}&apikey=c4b3701e"
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    jsonData = json.loads(data)["Search"]
-    return render_template("index.html" , page_name = "SEARCH RESULTS" , movieList = jsonData)
+@app.route("/search" , methods = ['GET'])
+# GET  - Insecure ( Search )
+# POST - Secure (Password , Credit Number )
+def search_results():
+    movieName  =  request.args.get("Moviequery")
+    try:
+        url =  f"http://www.omdbapi.com/?s={movieName}&apikey=c4b3701e"
+        url = url.replace(" ", "%20")
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        jsonData = json.loads(data)["Search"]
+        return render_template("index.html" , page_name = "SEARCH RESULTS" , movieList = jsonData , query = movieName)
+    except Exception as e:
+        print(e)
+        return f"NO INTERNET CONNECTION {e}"
+
+    
 
 
 
